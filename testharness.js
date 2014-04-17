5f596554c2c1f01ada14ae7746151ce8b9fdb5c7
Ms2ger <ms2ger@gmail.com>
2014-04-17 10:38:19 +0200

    Disable use of querySelectorAll (mozilla/servo#851).

--- testharness.js
+++ testharness.js
@@ -1466,6 +1466,7 @@ policies and contribution forms [3].
 
         log.appendChild(render(summary_template, {num_tests:tests.length}, output_document));
 
+        /* XXX #851
         forEach(output_document.querySelectorAll("section#summary label"),
                 function(element)
                 {
@@ -1488,7 +1489,7 @@ policies and contribution forms [3].
                                      style_element.parentNode.removeChild(style_element);
                                  }
                              });
-                });
+                });*/
 
         // This use of innerHTML plus manual escaping is not recommended in
         // general, but is necessary here for performance.  Using textContent
